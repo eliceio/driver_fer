f70526397295cff3490211b67799e7ca2ee0e8d9
leechungwan <cnddhks846@naver.com>
2018-08-09 14:54:52 +0900

    Update webcam_detect.py

--- webcam/webcam_detect.py
+++ webcam/webcam_detect.py
@@ -1,9 +1,15 @@
 import cv2
 import sys
+import numpy as np
+sys.path.append("../")
+import model.basenet as baseNet
 import detection_utilities as du
 
 windowName = 'Webcam Screen'
+weights_file = 'baseNet_weight.h5'
+FACE_SHAPE = (48, 48)
 
+emotion = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
 
 def getCameraStreaming():
     capture = cv2.VideoCapture(0)
@@ -20,30 +26,39 @@ def setDefaultCameraSetting():
     cv2.setWindowProperty(winname=windowName, prop_id=cv2.WINDOW_FULLSCREEN, prop_value=cv2.WINDOW_FULLSCREEN)
 
 
-def showScreenAndDetectFace(capture):
+def showScreenAndDetectFace(model, capture):
     while True:
         ret, frame = capture.read()
         face_coordinates = du.getFaceCoordinates(frame)
         refreshScreen(frame, face_coordinates)
+        # 얼굴을 detection 한 경우.
+        if face_coordinates is not None:
+            face = du.preprocess(frame, face_coordinates, FACE_SHAPE)
+            input_img = np.expand_dims(face, axis=0)
+            input_img = np.expand_dims(input_img, axis=-1)
+            result = model.predict(input_img)[0]
+            index = np.argmax(result)
+            print("Emotion : ", emotion[index])
         if cv2.waitKey(20) & 0xFF == ord('q'):
             break
 
 
 def refreshScreen(frame, face_coordinates):
-    cropped = frame
     if face_coordinates is not None:
-        cropped = du.crop_face(frame, face_coordinates)
-        cv2.imshow('crop', cropped)
         du.drawFace(frame, face_coordinates)
     cv2.imshow(windowName, frame)
 
 
+def buildNet(weights_file):
+    return baseNet.BaseNet(weights_file)
+
+
 def main():
     print("Start main() function.")
-
+    model = buildNet(weights_file)
     capture = getCameraStreaming()
     setDefaultCameraSetting()
-    showScreenAndDetectFace(capture)
+    showScreenAndDetectFace(model, capture)
 
 
 if __name__ == '__main__':
