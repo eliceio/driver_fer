ddc5617c7389b6f185c06101e568c60d9b230c5c
j8n <biasdrive@gmail.com>
2018-08-21 18:54:46 +0900

    load_save update. util for visualization / metric (gradCAM, confusion matrix)

--- model/load_save.py
+++ model/load_save.py
@@ -6,11 +6,74 @@ Created on Mon Aug 13 17:15:05 2018
 """
 
 import os
+import cv2
+import pandas as pd
+import numpy as np
 
 from keras.models import model_from_json
 from keras.models import load_model
+from keras.utils import np_utils
+from keras.utils.vis_utils import plot_model
 
 import autokeras as ak
+
+class_label = ['angry', 'disgust', 'fear','happy','sad','surprise','neutral']
+n_class = len(class_label)
+
+path = '/python/DDSA/fer/fer2013/'
+file_name = 'fer2013_2.csv'
+
+def load_data():
+    print("Start load_data")
+    fer = pd.read_csv(path + file_name)
+    fer_train = fer[fer.Usage == 'Training']
+    fer_test = fer[fer.Usage.str.contains('Test', na=False)]
+
+    x_train = np.array([list(map(int, x.split())) for x in fer_train['pixels'].values])
+    y_train = np.array(fer_train['emotion'].values)
+
+    x_test = np.array([list(map(int, x.split())) for x in fer_test['pixels'].values])
+    y_test = np.array(fer_test['emotion'].values)
+
+    x_train = normalize_x(x_train)
+    x_test = normalize_x(x_test)
+    y_train = np_utils.to_categorical(y_train, n_class)
+    y_test = np_utils.to_categorical(y_test, n_class)
+
+    return x_train, x_test, y_train, y_test
+
+# load data from csv for autokeras.
+# no need to use utils.to_categorical
+def load_data_for_ak():
+    print("Start load_data")
+    fer = pd.read_csv(path + file_name)
+    fer_train = fer[fer.Usage == 'Training']
+    fer_test = fer[fer.Usage.str.contains('Test', na=False)]
+
+    x_train = np.array([list(map(int, x.split())) for x in fer_train['pixels'].values])
+    y_train = np.array(fer_train['emotion'].values)
+
+    x_test = np.array([list(map(int, x.split())) for x in fer_test['pixels'].values])
+    y_test = np.array(fer_test['emotion'].values)
+
+    x_train = normalize_x(x_train)
+    x_test = normalize_x(x_test)
+
+    return x_train, x_test, y_train, y_test
+
+def normalize_x(data):
+    faces = []
+
+    for face in data:
+        face = face.reshape(48, 48) / 255.0
+        face = cv2.resize(face, (48, 48))
+        faces.append(face)
+
+    faces = np.asarray(faces)
+    faces = np.expand_dims(faces, -1)
+    return faces
+
+
 #from autokeras.preprocessor import OneHotEncoder, DataTransformer
 #from autokeras.constant import Constant
 ### To get autokeras,
@@ -22,7 +85,7 @@ import autokeras as ak
 
 ## ex) loaded_model = load_model_weight('sm_net.json', 'sm_net.h5')
 def load_model_weight(model_name, weight_name = None):
-    print('ddd')
+
     if weight_name ==None:  # which means, model & weight are saved in one file        
         loaded_model = load_model(model_name)
         print('Model & weight are loaded altogether.')
@@ -47,10 +110,11 @@ def save_model_weight(model, model_name = 'kafka', path = './'):
         m.write(model_json)
     
     model.save_weights(path + model_name + 'h5')     
+    # Save model as PNG (picture)
+    plot_model(model, to_file = model_name + '_net.png', show_shapes=True, show_layer_names=True)
     
     print('Model & Weight are saved separatedly. (model: .json) (weight: .h5)')
     
-
 ### load autokeras result, get best model (torch), convert and return keras model.
 
 def load_autokeras(path='/tmp/face/'):
@@ -70,3 +134,23 @@ def load_autokeras(path='/tmp/face/'):
     keras_model = graph.produce_keras_model()   # convert model from torch to keras
 
     return keras_model
+
+
+## convert csv file to face picture (png / jpg)
+def cvt_csv2face():
+
+    fer = pd.read_csv(path + file_name)
+    pictures = np.array([np.fromstring(x, np.uint8, sep=' ') for x in fer.pixels.values])
+    labels = np.array([np.fromstring(x, np.uint8) for x in fer.emotion.values]) #fromstring will be deprecated. change to frombuffer.
+    labels = labels[:,0]    # class label data    
+    
+    for _, j_class in enumerate(class_label):
+        if not os.path.exists(path + j_class):
+            os.makedirs(path + j_class)
+        
+    for i, picture in enumerate(pictures):
+        picture = picture.reshape((48, 48))
+        img_name = '%d.png' % i
+        #plt.show(picture)
+        cv2.imwrite(os.path.join(path, class_label[labels[i]], img_name), picture, [cv2.IMWRITE_PNG_COMPRESSION, 0])
+        #params for PNG, low value means low compression, big file size.(0 to 9)
---
+++ model/util.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 21 18:46:07 2018
+
+@author: 2014_Joon_IBS
+"""
+
+import numpy as np
+#np.seterr(divide='ignore', invalid='ignore')  #ignore divide by zero or non
+import matplotlib.pyplot as plt
+
+import matplotlib.patches as patches
+from skimage.measure import label, regionprops
+#import scipy
+from scipy import ndimage
+
+from sklearn.metrics import confusion_matrix #classification_report
+import itertools  # for confusion matrix plot
+
+from PIL import Image as pil_image
+import cv2
+
+#import tensorflow as tf
+#from tensorflow.python.framework import ops
+from keras import backend as K
+
+K.set_learning_phase(False)
+
+class_label = ['angry', 'disgust', 'fear', 'happy', 'sad','surprise', 'neutral']
+img_size = 48
+# to match the size, dimension, and color channel.
+    
+def preprocess(img_path):
+    
+    img = pil_image.open(img_path)
+    img = img.resize((img_size, img_size))
+    img_arr = np.asarray(img) / 255.
+    img_tensor = np.expand_dims(img_arr, 0)
+    img_tensor = np.expand_dims(img_tensor, 3)    
+    
+    return img_arr, img_tensor
+
+
+def grad_cam(model, img_path, class_idx, layer_idx):
+
+    img_arr, img_tensor = preprocess(img_path)
+
+    y_c = model.layers[-1].output.op.inputs[0][0, class_idx]  # final layer (before softmax)
+    layer_output = model.layers[layer_idx].output  # pick specific layer output (caution: conv layer only)
+    
+    grad = K.gradients(y_c, layer_output)[0]  # calculate gradient of y_c w.r.t. A_k from the conv layer output
+    gradient_fn = K.function([model.input], [layer_output, grad, model.layers[-1].output])
+    
+    conv_output, grad_val, predictions = gradient_fn([img_tensor])
+    conv_output, grad_val = conv_output[0], grad_val[0]
+    
+    weights = np.mean(grad_val, axis=(0, 1))
+    cam = np.dot(conv_output, weights)
+    cam = cv2.resize(cam, (img_size, img_size))
+    
+    # relu 
+    cam = np.maximum(cam, 0)    
+    cam = cam / cam.max()
+    
+    return img_arr, cam, predictions
+
+ # ex) img, cam, predictions = grad_cam(ak_net_0, img_path, class_idx, -13)
+
+def generate_bbox(img, cam, thr):
+    labeled, nr_objects = ndimage.label(cam > thr)
+    props = regionprops(labeled)
+    return props
+
+def plot_confusion_matrix(cm, classes,
+                          normalize = False,
+                          title = 'Confusion matrix',
+                          cmap = plt.cm.Blues):  
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis = 1)[:, np.newaxis]
+        print("normalized")
+    else:
+        print('without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap = cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment = "center",
+                 color = "white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+def make_confusion_matrix(model, x, y, normalize = True):
+    predicted = model.predict(x)
+
+    pred_list = []; actual_list = []
+    for i in predicted:
+        pred_list.append(np.argmax(i))
+    for i in y:
+        actual_list.append(np.argmax(i))
+
+    confusion_result = confusion_matrix(actual_list, pred_list)
+    plot_confusion_matrix(confusion_result, classes = class_label, normalize = normalize, title = 'Confusion_matrix')
+    return confusion_result
+    
+# ex) confusion_result = make_confusion_matrix(loaded_model, x_test, y_test, False)
