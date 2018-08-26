31b306eab5a7f0ccd706236f235617546065773b
JangMinYoung <min8540055@hanmail.net>
2018-08-27 00:30:47 +0900

    Add drowsy detection Code

--- webcam/detection_utilities.py
+++ webcam/detection_utilities.py
@@ -1,6 +1,10 @@
 import cv2
 import dlib
 from imutils import face_utils
+from scipy.spatial import distance as dist
+from threading import Thread
+import playsound
+import time
 
 # dlib을 위한 변수
 landmarks = 'shape_predictor_68_face_landmarks.dat'
@@ -14,6 +18,9 @@ WHITE_COLOR = (255, 255, 255)
 
 min_x, max_x, min_y, max_y = 0, 0, 0, 0
 rect = None
+
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["right_eye"]
 
 
 # Face Detection using Haar Cascades
@@ -100,23 +107,23 @@ def draw_landmark(frame):
 
     for (i, (x, y)) in enumerate(shape):
         cv2.circle(frame, (x, y), 1, (0, 0, 255), -1)
-        cv2.putText(frame, str(i + 1), (x - 10, y - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+        # cv2.putText(frame, str(i + 1), (x - 10, y - 10),
+        #             cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
 
 
 def checkFaceCoordinate(face_coordinates):
     global rect
     if len(face_coordinates) > 0:
-        print(face_coordinates[0])
         for face in face_coordinates:
             (x, y, w, h) = face_utils.rect_to_bb(face)
             if x in range(min_x, max_x) and y in range(min_y, max_y) \
                     and x + w in range(min_x, max_x) and y + h in range(min_y, max_y):
                 rect = face
-                return (x, y, w, h)
-    return ()
+                return face, (x, y, w, h)
+    return None, ()
     # case : haar cascade
     # for (x, y, w, h) in face_coordinates:
+
     #     x1 = int(width * 0.25)
     #     x2 = int(width * 0.5)
     #     y1 = int(height * 0.25)
@@ -125,3 +132,195 @@ def checkFaceCoordinate(face_coordinates):
     #         return True
     #     else:
     #         return False
+
+
+# 이 아래는 drowsy code.
+# 연속적으로 눈을 감은 횟수
+EYE_AR_CONSEC_FRAMES = 15
+consecutive_eyes_closed = 1
+
+# 전체 눈 깜빡임 횟수
+TOTAL = 0
+# 눈 뜬 여부
+eye_open = True
+# 연속적으로 눈 깜빡이는 주기가 느린 횟수
+count_drowsy_detection = 0  # 횟수 저장
+consecutive_drowsy = 4  # 기준이 되는 횟수
+
+# 눈 인식 되지 않는 시간##
+eye_not_recognition_time = 0
+
+# 사용자의 눈 크기
+user_eye = 0
+repeat = 0
+Sleeping_eye = 0
+
+# 졸음이 깰때까지 webcam에 문구를 띄우기 위해 추가 된 변수
+Slow_blinking = False
+COUNTER = 0
+ALARM_ON = False
+start_time = 0
+end_time = 0
+
+
+# 5초동안 사용자 눈 크기 계산
+def eye_size_cal(ear, frame,camera_width):
+    global repeat, user_eye, Sleeping_eye
+    user_eye += ear
+    print("ear:" + str(ear))
+    print("user : " + str(user_eye))
+
+    cv2.putText(frame, "eye size: {:.2f}".format(user_eye / (repeat - 40)), (int(camera_width*0.7), 30),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
+    if repeat == 45:
+        print("사용자 눈 크기 " + str(user_eye / 5))
+        user_eye = (user_eye / 5)
+        Sleeping_eye = (user_eye * 0.75)
+        print("자는 눈 계산" + str(Sleeping_eye))
+    return user_eye, Sleeping_eye
+
+
+def draw_eye(frame):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    shape = predictor(gray, rect)
+    shape = face_utils.shape_to_np(shape)
+
+    left_eye = shape[lStart:lEnd]
+    right_eye = shape[rStart:rEnd]
+    left_ear = eye_aspect_ratio(left_eye)
+    right_ear = eye_aspect_ratio(right_eye)
+
+    ear = (left_ear + right_ear) / 2.0
+
+    left_eye_hull = cv2.convexHull(left_eye)
+    right_eye_hull = cv2.convexHull(right_eye)
+    cv2.drawContours(frame, [left_eye_hull], -1, (0, 255, 0), 1)
+    cv2.drawContours(frame, [right_eye_hull], -1, (0, 255, 0), 1)
+    return ear
+
+
+def eye_aspect_ratio(eye):
+    # compute the euclidean distances between the two sets of
+    # vertical eye landmarks (x, y)-coordinates
+    A = dist.euclidean(eye[1], eye[5])
+    B = dist.euclidean(eye[2], eye[4])
+
+    # compute the euclidean distance between the horizontal
+    # eye landmark (x, y)-coordinates
+    C = dist.euclidean(eye[0], eye[3])
+
+    # compute the eye aspect ratio
+    ear = (A + B) / (2.0 * C)
+
+    # return the eye aspect ratio
+    return ear
+
+
+# 눈 크기를 측정하기 위해 처음 시작을 알리는 webcam 출력값
+def start(frame):
+    global repeat
+    check_detect_area(frame)
+    if repeat >= 1 and repeat <= 10:
+        cv2.putText(frame, "Look at the camera for five seconds.", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.7,
+                    (255, 0, 0), 2)
+    elif repeat >= 11 and repeat <= 20:
+        cv2.putText(frame, "3", (150, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.7,
+                    (255, 0, 0), 2)
+    elif repeat >= 21 and repeat <= 30:
+        cv2.putText(frame, "2", (150, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.7,
+                    (255, 0, 0), 2)
+    elif repeat >= 31 and repeat <= 40:
+        cv2.putText(frame, "1", (150, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.7,
+                    (255, 0, 0), 2)
+
+
+def sound_alarm(path):
+    # play an alarm sound
+    playsound.playsound(path)
+
+
+def warning(frame):
+    global ALARM_ON
+    # if the alarm is not on, turn it on
+    if not ALARM_ON:
+        ALARM_ON = True
+
+        t = Thread(target=sound_alarm,
+                   args=("alarm.WAV",))
+        t.deamon = True
+        t.start()
+
+    # draw an alarm on the frame
+    cv2.putText(frame, "Drowsy Warning!", (10, 30),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+    return ALARM_ON
+
+
+def drowsy_detection(frame, face,camera_width):
+    global repeat, ALARM_ON, eye_not_recognition_time, user_eye, Sleeping_eye, eye_open, COUNTER, end_time, start_time, count_drowsy_detection, Slow_blinking, TOTAL
+    repeat += 1
+
+    # 눈 인식이 20초 이상 되지 않을 경우 알림 울리기
+    if not face:
+        eye_not_recognition_time += 1
+    if eye_not_recognition_time >= 20:
+        ALARM_ON = warning(frame)
+    if repeat <= 40:
+        start(frame)
+
+    if face is not None:
+        eye_not_recognition_time = 0
+        ear = draw_eye(frame)
+
+        # 사용자의 평균 눈 크기 구하기
+        if repeat in range(41, 46):
+            user_eye, Sleeping_eye = eye_size_cal(ear, frame,camera_width)
+        elif repeat in range(46, 56):
+            cv2.putText(frame, "Start!", (150, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
+
+        elif repeat >= 56:
+            # check to see if the eye aspect ratio is below the blink
+            # threshold, and if so, increment the blink frame counter
+            if ear < Sleeping_eye:
+                if eye_open:
+                    print("눈감음")
+                    start_time = time.time()
+                    eye_open = False
+
+                COUNTER += 1
+                # if the eyes were closed for a sufficient number of
+                # then sound the alarm
+                if COUNTER >= EYE_AR_CONSEC_FRAMES:
+                    ALARM_ON = warning(frame)
+
+            # otherwise, the eye aspect ratio is not below the blink
+            # threshold, so reset the counter and alarm
+            else:
+                if not eye_open:
+                    end_time = time.time()
+                    print("눈 뜸 " + str(end_time - start_time))
+                    if (end_time - start_time) >= float(0.4):
+                        count_drowsy_detection += 1
+                    else:
+                        count_drowsy_detection = 0
+                    if count_drowsy_detection >= consecutive_drowsy:
+                        if not Slow_blinking:
+                            ALARM_ON = warning(frame)
+                            Slow_blinking = True
+                    if Slow_blinking:
+                        print("slow_blink")
+                        Slow_blinking = False
+                    eye_open = True
+                if COUNTER >= consecutive_eyes_closed:
+                    TOTAL += 1
+                COUNTER = 0
+                ALARM_ON = False
+
+            # draw the computed eye aspect ratio on the frame to help
+            cv2.putText(frame, "EAR: {:.2f}".format(ear), (int(camera_width*0.8), 30),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
+
+            # 눈 깜빡인 횟수 화면 출력
+            cv2.putText(frame, "Blinks: {}".format(TOTAL), (int(camera_width*0.8), 50),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
